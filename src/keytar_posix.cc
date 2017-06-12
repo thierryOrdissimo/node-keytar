8bc54f34683f222b2ea6fd38b1e5a318dcfe5cde
Thierry <thierry@substantiel.fr>
2017-06-12 15:34:58 +0200

    Create keytar_posix.cc
    Compatible squeeze and wheezy

--- src/keytar_posix.cc
+++ src/keytar_posix.cc
@@ -1,123 +1,96 @@
 #include "keytar.h"
 
-#include <libsecret/secret.h>
+#include <gnome-keyring.h>
 #include <stdio.h>
 
 namespace keytar {
 
 namespace {
-
-static const SecretSchema schema = {
-  "org.freedesktop.Secret.Generic", SECRET_SCHEMA_NONE, {
-    { "service", SECRET_SCHEMA_ATTRIBUTE_STRING },
-    { "account", SECRET_SCHEMA_ATTRIBUTE_STRING }
-  }
-};
-
+    const GnomeKeyringPasswordSchema kGnomeSchema = {
+      GNOME_KEYRING_ITEM_GENERIC_SECRET, {
+        { "service", GNOME_KEYRING_ATTRIBUTE_TYPE_STRING },
+        { "account", GNOME_KEYRING_ATTRIBUTE_TYPE_STRING },
+        { NULL }
+      }
+    };
 }  // namespace
 
 KEYTAR_OP_RESULT SetPassword(const std::string& service,
                              const std::string& account,
                              const std::string& password,
                              std::string* errStr) {
-  GError* error = NULL;
-
-  secret_password_store_sync(
-    &schema,                            // The schema.
-    SECRET_COLLECTION_DEFAULT,          // Default collection.
-    (service + "/" + account).c_str(),  // The label.
-    password.c_str(),                   // The password.
-    NULL,                               // Cancellable. (unneeded)
-    &error,                             // Reference to the error.
-    "service", service.c_str(),
-    "account", account.c_str(),
-    NULL);                              // End of arguments.
-
-  if (error != NULL) {
-    *errStr = std::string(error->message);
-    g_error_free(error);
+  GnomeKeyringResult result = gnome_keyring_store_password_sync(
+      &kGnomeSchema,
+      NULL,  // Default keyring.
+      (service + "/" + account).c_str(),  // Display name.
+      password.c_str(),
+      "service", service.c_str(),
+      "account", account.c_str(),
+      NULL);
+  if (result != GNOME_KEYRING_RESULT_OK) {
+    *errStr = std::string(gnome_keyring_result_to_message(result));
     return FAIL_ERROR;
   }
 
   return SUCCESS;
 }
-
+  
 KEYTAR_OP_RESULT GetPassword(const std::string& service,
                              const std::string& account,
                              std::string* password,
                              std::string* errStr) {
-  GError* error = NULL;
-
-  gchar* raw_password = secret_password_lookup_sync(
-    &schema,                            // The schema.
-    NULL,                               // Cancellable. (unneeded)
-    &error,                             // Reference to the error.
-    "service", service.c_str(),
-    "account", account.c_str(),
-    NULL);                              // End of arguments.
-
-  if (error != NULL) {
-    *errStr = std::string(error->message);
-    g_error_free(error);
+  gchar* raw_passwords;
+  GnomeKeyringResult result = gnome_keyring_find_password_sync(
+     &kGnomeSchema,
+     &raw_passwords,
+     "service", service.c_str(),
+     "account", account.c_str(),
+     NULL);
+  if (result != GNOME_KEYRING_RESULT_OK) {
+    *errStr = std::string(gnome_keyring_result_to_message(result));
     return FAIL_ERROR;
   }
-
-  if (raw_password == NULL)
+  if (raw_passwords == NULL)
     return FAIL_NONFATAL;
-
-  *password = raw_password;
-  secret_password_free(raw_password);
+  *password = raw_passwords;
+  gnome_keyring_free_password(raw_passwords);
   return SUCCESS;
 }
 
 KEYTAR_OP_RESULT DeletePassword(const std::string& service,
                                 const std::string& account,
                                 std::string* errStr) {
-  GError* error = NULL;
+  GnomeKeyringResult result = gnome_keyring_delete_password_sync(
+      &kGnomeSchema,
+      "service", service.c_str(),
+      "account", account.c_str(),
+      NULL);
 
-  gboolean result = secret_password_clear_sync(
-    &schema,                            // The schema.
-    NULL,                               // Cancellable. (unneeded)
-    &error,                             // Reference to the error.
-    "service", service.c_str(),
-    "account", account.c_str(),
-    NULL);                              // End of arguments.
-
-  if (error != NULL) {
-    *errStr = std::string(error->message);
-    g_error_free(error);
+  if (result != GNOME_KEYRING_RESULT_OK) {
+    *errStr = std::string(gnome_keyring_result_to_message(result));
     return FAIL_ERROR;
   }
-
-  if (!result)
-    return FAIL_NONFATAL;
-
   return SUCCESS;
 }
 
 KEYTAR_OP_RESULT FindPassword(const std::string& service,
                               std::string* password,
                               std::string* errStr) {
-  GError* error = NULL;
-
-  gchar* raw_password = secret_password_lookup_sync(
-    &schema,                            // The schema.
-    NULL,                               // Cancellable. (unneeded)
-    &error,                             // Reference to the error.
-    "service", service.c_str(),
-    NULL);                              // End of arguments.
-
-  if (error != NULL) {
-    *errStr = std::string(error->message);
-    g_error_free(error);
+  gchar* raw_passwords;
+  GnomeKeyringResult result = gnome_keyring_find_password_sync(
+        &kGnomeSchema,
+        &raw_passwords,
+        "service", service.c_str(),
+        NULL);
+  if (result != GNOME_KEYRING_RESULT_OK){
+    *errStr = std::string(gnome_keyring_result_to_message(result));
     return FAIL_ERROR;
   }
-
-  if (raw_password == NULL)
+  if (raw_passwords == NULL)
     return FAIL_NONFATAL;
 
-  *password = raw_password;
-  secret_password_free(raw_password);
+  *password = raw_passwords;
+  gnome_keyring_free_password(raw_passwords);
   return SUCCESS;
 }
 
